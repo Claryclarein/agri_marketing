50581b68ec22da64e71ff1281c07c33bcb740aa2
Clare Lusiche <clarelusiche@gmail.com>
2022-11-10 22:41:47 +0300

    Inputing text

--- agri_marketing/lib/screen/home_page.dart
+++ agri_marketing/lib/screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightGreen,
+      backgroundColor: Colors.white,
+      body: Container(
+        width: Get.width,
+        height: Get.height,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: Text("AGRI_MARKETING",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.greenAccent,
+                      fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
